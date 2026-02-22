4ba284cebe9611d4b0083165d2e3a5d06746286b
jeine doe <kyryichuk.yevheniia@gmail.com>
2026-02-22 21:42:11 +0200

    Home work 26 has been completed

--- playwright.config.js
+++ playwright.config.js
@@ -1,38 +1,27 @@
 // @ts-check
 const { defineConfig, devices } = require("@playwright/test");
+require("dotenv").config();
 
 module.exports = defineConfig({
   testDir: "./tests",
 
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 2,
-
   reporter: "html",
 
   use: {
-    baseURL: "https://qauto.forstudy.space",
+    baseURL: process.env.BASE_URL,
+
     httpCredentials: {
-      username: "guest",
-      password: "welcome2qauto",
+      username: process.env.HTTP_USERNAME,
+      password: process.env.HTTP_PASSWORD,
     },
+
     headless: true,
     trace: "retain-on-failure",
   },
 
   projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
   ],
 });
